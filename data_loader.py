377ffffa2bb03b68276ea32bccbbfcbc6565e846
Dual2Core <patryk.kobielak@gmail.com>
2017-03-16 21:41:30 +0100

    shit

--- data_loader.py
+++ data_loader.py
@@ -21,16 +21,24 @@ def get_train_data_for_localization():
     bounding_box_data = []
 
     # Create X and Y
+    counter = 1
     for image_path in filepaths:
         image = cv2.imread(image_path)
         image = cv2.resize(image, (1280, 720))
 
         image_data.append(image)
+        counter = counter + 1
 
         filename = os.path.basename(image_path)
         bounding_box_data.append(annotations[filename])
-    
-    # Return X and Y for regression
+
+        if counter %100 == 0:
+            # Return X and Y for regression
+            print("Wyslalem paczke")
+            yield image_data, bounding_box_data
+            image_data = []
+            bounding_box_data = []
+
     return image_data, bounding_box_data
 
 get_train_data_for_localization()
--- train_localization_network.py
+++ train_localization_network.py
@@ -9,23 +9,24 @@ from tflearn.layers.estimator import regression
 from tflearn.data_preprocessing import ImagePreprocessing
 from tflearn.data_augmentation import ImageAugmentation
 
+steps = 200
+
 # Data loading and preprocessing
-X, Y = data_loader.get_train_data_for_localization()
+data_generator = data_loader.get_train_data_for_localization()
+X, Y = next(data_generator)
 
 # Real-time data preprocessing
-img_prep = ImagePreprocessing()
-img_prep.add_featurewise_zero_center()
-img_prep.add_featurewise_stdnorm()
+#img_prep = ImagePreprocessing()
+#img_prep.add_featurewise_zero_center()
+#img_prep.add_featurewise_stdnorm()
 
 # Real-time data augmentation
-img_aug = ImageAugmentation()
+#img_aug = ImageAugmentation()
 #img_aug.add_random_flip_leftright()
 #img_aug.add_random_rotation(max_angle=25.)
 
 # Convolutional network building
-network = input_data(shape=[None, 1280, 720, 3],
-                     data_preprocessing=img_prep,
-                     data_augmentation=img_aug)
+network = input_data(shape=[None, 1280, 720, 3])
 network = conv_2d(network, 32, 3, activation='relu')
 network = max_pool_2d(network, 2)
 network = conv_2d(network, 64, 3, activation='relu')
@@ -40,5 +41,7 @@ network = regression(network, optimizer='adam',
 
 # Train using classifier
 model = tflearn.DNN(network, tensorboard_verbose=0)
-model.fit(X, Y, n_epoch=50, shuffle=True, validation_set=(X_test, Y_test),
-          show_metric=True, batch_size=96, run_id='cifar10_cnn')
+
+for step in range(1, steps):
+    model.fit(X, Y, n_epoch=10, shuffle=True, validation_set=(X, Y),
+            show_metric=True, batch_size=100, run_id='cifar10_cnn')
