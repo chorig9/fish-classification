b4f9537ea884ce72aef0ae571c2b2dd7180af8af
Igor Chorazewicz <chorig9@gmail.com>
2017-03-18 18:55:29 +0100

    added resized loader

--- train_localization_network.py
+++ train_localization_network.py
@@ -1,46 +1,41 @@
 from __future__ import division, print_function, absolute_import
 
+import numpy as np
 import tflearn
-import data_loader
-from tflearn.data_utils import shuffle, to_categorical
+import tensorflow as tf
 from tflearn.layers.core import input_data, dropout, fully_connected
 from tflearn.layers.conv import conv_2d, max_pool_2d
 from tflearn.layers.estimator import regression
-from tflearn.data_preprocessing import ImagePreprocessing
-from tflearn.data_augmentation import ImageAugmentation
+import resized_loader
 
 steps = 200
+X, Y = resized_loader.get_resized_input_data()
 
-# Data loading and preprocessing
-data_generator = data_loader.get_train_data_for_localization()
-X, Y = next(data_generator)
+def l2_loss(y_pred, y_true):
+    return tf.reduce_sum(tf.square(y_pred - y_true))
 
-# Real-time data preprocessing
-#img_prep = ImagePreprocessing()
-#img_prep.add_featurewise_zero_center()
-#img_prep.add_featurewise_stdnorm()
-
-# Real-time data augmentation
-#img_aug = ImageAugmentation()
-#img_aug.add_random_flip_leftright()
-#img_aug.add_random_rotation(max_angle=25.)
 
 # Convolutional network building
-network = input_data(shape=[None, 100, 100, 3])
-network = conv_2d(network, 32, 3, activation='relu')
+network = input_data(shape=[None, 144, 256, 3])
+network = conv_2d(network, 16, 3, activation='relu')
 network = max_pool_2d(network, 2)
 network = conv_2d(network, 64, 3, activation='relu')
+network = max_pool_2d(network, 2)
+network = conv_2d(network, 64, 3, activation='relu')
+network = max_pool_2d(network, 2)
+network = conv_2d(network, 64, 3, activation='relu')
+network = max_pool_2d(network, 2)
 network = conv_2d(network, 64, 3, activation='relu')
 network = max_pool_2d(network, 2)
 network = fully_connected(network, 512, activation='relu')
 network = dropout(network, 0.5)
 network = fully_connected(network, 4, activation='linear')
 network = regression(network, optimizer='adam',
-                     learning_rate=0.001, loss='mean_square')
+                     learning_rate=0.001, loss=l2_loss)
 
 # Train using classifier
 model = tflearn.DNN(network, tensorboard_verbose=0)
 
 for step in range(1, steps):
     model.fit(X, Y, n_epoch=10, shuffle=True, validation_set=(X, Y),
-            show_metric=True, batch_size=100, run_id='cifar10_cnn')
+            show_metric=True, batch_size=100, run_id='bounding_box_network')
